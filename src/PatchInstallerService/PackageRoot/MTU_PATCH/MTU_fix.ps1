92767e7156d3c6017714247405f001d2f9296675
Diane Hadley <dihadley@microsoft.com>
2018-11-12 16:41:22 -0800

    Update MTU patch to support overlay networks

--- src/PatchInstallerService/PackageRoot/MTU_PATCH/MTU_fix.ps1
+++ src/PatchInstallerService/PackageRoot/MTU_PATCH/MTU_fix.ps1
@@ -1,12 +1,36 @@
+$overlayinterfaceindices = @()
+$overlaynetworks = @(Get-HNSNetwork | ? {$_.Type -match "Overlay"})
+
+for ($i = 0; $i -lt $overlaynetworks.Length; $i++){
+    if (!$overlaynetworks[$i].NetworkAdapterName) {
+        continue
+    } 
+
+    $adapter = Get-NetAdapter -Name $overlaynetworks[$i].NetworkAdapterName    
+    # Virtual adapter has same MAC address as its corresponding physical adapter
+    $vadapter = Get-NetAdapter | ? {$_.MacAddress -match $adapter.MacAddress -and $_.virtual -eq $true}
+    $overlayinterfaceindices += $vadapter.ifIndex
+}  
+
 Write-Output ("Getting IPv4 interfaces list ...")
 $interfaces = @(Get-NetIPInterface -AddressFamily IPv4)
 
 for ($i = 0; $i -lt $interfaces.Length; $i++) {
-    if ( $interfaces[$i].NlMtu -lt 1500 ) {
-        Write-Output ('Found IPv4 interface named ' + $interfaces[$i].InterfaceAlias + ' with MTU size less than 1500.')
+    # Default MTU size for all non-overlay networks
+    $desiredMTU = 1500     
+    # Used by output message to specify if overlay network
+    $networktypeclarification = ""       
+    # If interface index is in this list, we know the interface is used by an overlay network
+    if ($overlayinterfaceindices.contains($interfaces[$i].ifIndex)) {
+        # Default MTU size for overlay networks
+        $desiredMTU = 1450
+        $networktypeclarification = " used by an overlay network"
+    }
 
+    if ($interfaces[$i].NlMtu -lt $desiredMTU) {
+        Write-Output ('Found IPv4 interface named ' + $interfaces[$i].InterfaceAlias + $networktypeclarification + ' with MTU size less than ' + $desiredMTU + '.')
         Try {
-            $command = 'Set-NetIPInterface -InterfaceIndex ' + $interfaces[$i].InterfaceIndex + ' -NlMtuBytes 1500'
+            $command = 'Set-NetIPInterface -InterfaceIndex ' + $interfaces[$i].InterfaceIndex + ' -NlMtuBytes ' + $desiredMTU
             Write-Output ('Executing command ' + $command)
             Invoke-Expression -Command $command
         }
--- src/PatchInstallerService/PackageRoot/MTU_PATCH/verify.ps1
+++ src/PatchInstallerService/PackageRoot/MTU_PATCH/verify.ps1
@@ -1,2 +1,33 @@
-$interfaces = @(Get-NetIPInterface -AddressFamily IPv4 | ? {$_.NlMtu -lt 1500}).Count
-exit $interfaces
+$overlayinterfaceindices = @()
+$overlaynetworks = @(Get-HNSNetwork | ? {$_.Type -match "Overlay"})
+
+for ($i = 0; $i -lt $overlaynetworks.Length; $i++){
+    if (!$overlaynetworks[$i].NetworkAdapterName) {
+        continue
+    } 
+
+    $adapter = Get-NetAdapter -Name $overlaynetworks[$i].NetworkAdapterName    
+    # Virtual adapter has same MAC address as its corresponding physical adapter
+    $vadapter = Get-NetAdapter | ? {$_.MacAddress -match $adapter.MacAddress -and $_.virtual -eq $true}
+    $overlayinterfaceindices += $vadapter.ifIndex
+}  
+
+Write-Output ("Getting IPv4 interfaces list ...")
+$interfaces = @(Get-NetIPInterface -AddressFamily IPv4)
+
+$numbadinterfaces = 0
+for ($i = 0; $i -lt $interfaces.Length; $i++) {   
+    # Default MTU size for all non-overlay networks
+    $desiredMTU = 1500        
+    # If interface index is in this list, we know the interface is used by an overlay network
+    if ($overlayinterfaceindices.contains($interfaces[$i].ifIndex)) {
+        # Default MTU size for overlay networks
+        $desiredMTU = 1450
+    }
+
+    if ($interfaces[$i].NlMtu -lt $desiredMTU) {
+        $numbadinterfaces++
+    }
+}
+
+exit $numbadinterfaces
